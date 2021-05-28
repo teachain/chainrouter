6b54adc64ad5b40ccf969ea7114035d4f603a999
teachain <281477138@qq.com>
2021-05-28 17:50:31 +0800

    transaction params

---
+++ stub/transaction_params.go
@@ -0,0 +1,10 @@
+package stub
+
+type TransactionParams struct {
+	TransactionRequest TransactionRequest
+	Data               string
+	From               string
+	To                 string
+	Abi                string
+	SubType            string
+}
